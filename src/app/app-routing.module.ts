a66a1f03187431b9518f8d46ecca6b9c31cc2833
Djtooth <davidjnelson17@gmail.com>
2018-06-06 09:37:39 -0400

    Last practice change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes } from '@angular/router';
 import { ShowMovieComponent } from './show-movie/show-movie.component';
 import { ItemsComponent } from './items/items.component';
+import { HomeComponent } from './home/home.component';
 
 const routes : Routes = [
   {path: 'movie/:movie_id', component: ShowMovieComponent},
-  {path: 'movies', component: ItemsComponent}
+  {path: 'movies', component: ItemsComponent},
+  {path: 'home', component: HomeComponent}
 ]
 
 @NgModule({
